801f3a76b1caf1278e7da4f510ae48a7bbd41407
Anri Liy <anri@Anris-MacBook-Pro.local>
2018-02-20 12:08:22 -0600

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,10 @@ Rails.application.routes.draw do
   resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get "signup", to: 'users#new'
+  #post 'users', to: 'users#new'
 
+  resources :users, except: [:new]
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 

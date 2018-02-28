74ca9b4fd250cd3a89a0cfc6559e78705f303fb0
Anri Liy <anri@Anris-MacBook-Pro.local>
2018-02-28 17:02:39 -0600

    Add users listing and updateshow article view

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe User, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
 
   it "is valid with a name, email, and password"
-
-  it "has a number"
+    user = User.new(
+      username: "Star",
+      email: "star@example.com",
+      password: "1234"
+    )
+    expect(username).to be_valid
 end

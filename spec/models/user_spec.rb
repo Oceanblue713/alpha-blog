cd5132b42f1f619454a9a5292bc9866ed12872f8
Anri Liy <anri@Anris-MacBook-Pro.local>
2018-03-02 13:08:05 -0600

    Add pagination to view

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  pending "add some examples to (or delete) #{__FILE__}"
 
-  it "is valid with a name, email, and password"
+  it "is valid with a username, email, and password"
+
     user = User.new(
       username: "Star",
       email: "star@example.com",
       password: "1234"
     )
-    expect(username).to be_valid
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a username"
+
+  it "is invalid without an email"
+
+  it "is invalid without a password"
+
+  it "returns a username's username as a string"
 end
